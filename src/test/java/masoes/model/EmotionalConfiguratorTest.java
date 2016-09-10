fa159f9acb220e9e775ce3145da6e8c0c9c077ee
Saúl Piña <sauljabin@gmail.com>
2016-09-10 14:00:40 -0500

    Agregado test para settings

---
+++ src/test/java/masoes/model/EmotionalConfiguratorTest.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2016 Saúl Piña <sauljabin@gmail.com>
+ * License GPLv3 <https://www.gnu.org/licenses/gpl-3.0.html>
+ * Please see the LICENSE.txt file
+ */
+
+package masoes.model;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class EmotionalConfiguratorTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}
---
+++ src/test/java/masoes/setting/SettingsTest.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2016 Saúl Piña <sauljabin@gmail.com>
+ * License GPLv3 <https://www.gnu.org/licenses/gpl-3.0.html>
+ * Please see the LICENSE.txt file
+ */
+
+package masoes.setting;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SettingsTest {
+
+    @Test
+    public void shouldReturnCorrectValue() {
+        SettingsLoader expectedSettingsLoader = SettingsLoader.getInstance();
+        assertThat(expectedSettingsLoader.get("app.name"), is(Settings.APP_NAME.getValue()));
+    }
+
+}
