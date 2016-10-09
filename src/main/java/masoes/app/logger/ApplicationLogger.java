3410a322fe857705667e531c0962e90a7997da81
Saúl Piña <sauljabin@gmail.com>
2016-10-08 20:48:32 -0500

    Actualizado logger

--- src/main/java/masoes/app/logger/ApplicationLogger.java
+++ src/main/java/masoes/app/logger/ApplicationLogger.java
@@ -34,7 +34,7 @@ public class ApplicationLogger {
 
     public void cantNotStartApplication(Exception exception) {
         LogWriter.newInstance()
-                .message("Could not start the application -> %s")
+                .message("Could not start the application [%s]")
                 .args(exception.getMessage())
                 .exception(exception)
                 .error(logger);
