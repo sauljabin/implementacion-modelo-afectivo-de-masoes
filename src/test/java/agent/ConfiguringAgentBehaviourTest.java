270672357c279c6786b27927e1b2f8e96966fb4a
Saúl Piña <sauljabin@gmail.com>
2017-02-01 11:03:01 -0500

    Se mueve cabecera

--- src/test/java/agent/ConfiguringAgentBehaviourTest.java
+++ src/test/java/agent/ConfiguringAgentBehaviourTest.java
@@ -1,10 +1,10 @@
-package agent;
-
 /*
  * Copyright (c) 2016 Saúl Piña <sauljabin@gmail.com>
  * License GPLv3 <https://www.gnu.org/licenses/gpl-3.0.html>
  * Please see the LICENSE.txt file
  */
+
+package agent;
 
 import jade.core.Agent;
 import ontology.configurable.ConfigurableOntology;
