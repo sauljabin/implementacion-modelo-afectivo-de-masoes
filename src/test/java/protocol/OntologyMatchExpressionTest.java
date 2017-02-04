d5b3957033a9f18036ce986725176c11976c85a9
Saúl Piña <sauljabin@gmail.com>
2017-02-04 18:10:33 -0500

    Mejora en matcher de mesajes

---
+++ src/test/java/protocol/OntologyMatchExpressionTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+/*
+ * Copyright (c) 2016 Saúl Piña <sauljabin@gmail.com>
+ * License GPLv3 <https://www.gnu.org/licenses/gpl-3.0.html>
+ * Please see the LICENSE.txt file
+ */
+
+public class OntologyMatchExpressionTest {
+
+}
