c4e5abaa08153ab72427adedad4e253b27c23079
Saúl Piña <sauljabin@gmail.com>
2017-02-28 10:49:34 -0500

    Se agrega constantes en ontología masoes

--- src/main/java/ontology/masoes/MasoesOntology.java
+++ src/main/java/ontology/masoes/MasoesOntology.java
@@ -16,6 +16,10 @@ public class MasoesOntology extends BeanOntology {
     public static final String ACTION_GET_EMOTIONAL_STATE = "GetEmotionalState";
     public static final String ACTION_EVALUATE_STIMULUS = "EvaluateStimulus";
     public static final String ACTION_NOTIFY_ACTION = "NotifyAction";
+    public static final String ACTION_CREATE_OBJECT = "CreateObject";
+    public static final String ACTION_DELETE_OBJECT = "DeleteObject";
+    public static final String ACTION_GET_OBJECT = "GetObject";
+    public static final String ACTION_UPDATE_OBJECT = "UpdateObject";
     private static MasoesOntology INSTANCE;
 
     private MasoesOntology() {
