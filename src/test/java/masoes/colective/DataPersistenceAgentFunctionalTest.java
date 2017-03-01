b409fe193c0fd0e53eddd2a05c13ab39d8d43fb4
Saúl Piña <sauljabin@gmail.com>
2017-03-01 14:43:01 -0500

    Se inicia pruebas funcionales para agente de persistencia

---
+++ src/test/java/masoes/colective/DataPersistenceAgentFunctionalTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2016 Saúl Piña <sauljabin@gmail.com>
+ * License GPLv3 <https://www.gnu.org/licenses/gpl-3.0.html>
+ * Please see the LICENSE.txt file
+ */
+
+package masoes.colective;
+
+import jade.core.AID;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import ontology.masoes.MasoesOntology;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import test.FunctionalTest;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
+import static org.junit.Assert.assertThat;
+
+public class DataPersistenceAgentFunctionalTest extends FunctionalTest {
+
+    private AID settingsAgentAID;
+
+    @Before
+    public void setUp() {
+        settingsAgentAID = createAgent(DataPersistenceAgent.class, null);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        killAgent(settingsAgentAID);
+    }
+
+    @Test
+    public void shouldGetAllServicesFromDF() {
+        List<ServiceDescription> services = services(settingsAgentAID);
+        List<String> results = services.stream().map(ServiceDescription::getName).collect(Collectors.toList());
+        assertThat(results, hasItems(
+                MasoesOntology.ACTION_GET_OBJECT,
+                MasoesOntology.ACTION_CREATE_OBJECT,
+                MasoesOntology.ACTION_DELETE_OBJECT,
+                MasoesOntology.ACTION_UPDATE_OBJECT
+        ));
+    }
+
+}
