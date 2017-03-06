2ed6e23c4d73ed7c41cca9338189cd81ef76e43f
Saúl Piña <sauljabin@gmail.com>
2017-03-05 19:13:36 -0500

    Pruebas de observer

---
+++ src/test/java/logger/LogWriterObserverTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2016 Saúl Piña <sauljabin@gmail.com>
+ * License GPLv3 <https://www.gnu.org/licenses/gpl-3.0.html>
+ * Please see the LICENSE.txt file
+ */
+
+package logger;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.event.Level;
+
+import java.util.Observable;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+public class LogWriterObserverTest {
+
+    private LoggerHandler loggerHandlerMock;
+    private LogWriterObserver logWriterObserver;
+
+    @Before
+    public void setUp() {
+        loggerHandlerMock = mock(LoggerHandler.class);
+        logWriterObserver = new LogWriterObserver(loggerHandlerMock);
+    }
+
+    @Test
+    public void shouldInvokeLoggerHandler() throws Exception {
+        String expectedObject = "expectedObject";
+        LogWriterNotification notification = new LogWriterNotification(Level.INFO, expectedObject);
+        logWriterObserver.update(mock(Observable.class), notification);
+        verify(loggerHandlerMock).handleMessage(Level.INFO, expectedObject);
+    }
+
+    @Test
+    public void shouldNotInvokeLoggerHandler() throws Exception {
+        logWriterObserver.update(mock(Observable.class), new Object());
+        verify(loggerHandlerMock, never()).handleMessage(any(), any());
+    }
+
+}
